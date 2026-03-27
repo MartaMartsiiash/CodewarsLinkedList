5987071b0e9341b4afab20af6ee68d8f03449ce0
MartaMartsiiash <martsiiash.pn@ucu.edu.ua>
2026-03-27 22:25:01 +0200

    Add reverse solution and tests screenshot

---
+++ reverse/reverse.py
@@ -0,0 +1,17 @@
+"""Reverse"""
+class Node(object):
+    """Node"""
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+def reverse(head):
+    """reverse"""
+    # your code goes here.
+    if head is None or head.next is None:
+        return head
+    new_head = reverse(head.next)
+    head.next.next = head
+    head.next = None
+
+    return new_head
